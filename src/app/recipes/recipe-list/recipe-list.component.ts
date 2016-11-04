8acf1d2a442682c7d5a5ae72cc7b1cb657ac497f
Charles Heidemann <c.heidemann@2g.tj.rs>
2016-11-04 15:47:15 -0200

    First commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+import { Recipe } from '../recipe';
+
+@Component({
+  selector: 'rb-recipe-list',
+  templateUrl: './recipe-list.component.html'
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [];
+
+  recipe: Recipe = new Recipe('Dumy', 'Dumy', 'favicon.ico');
+
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
+  }
+
+}
---
+++ src/app/recipes/recipe.ts
@@ -0,0 +1,7 @@
+export class Recipe {
+
+    public constructor(public name:string, public description:string, public imagePath:string ){
+
+    }
+
+}
